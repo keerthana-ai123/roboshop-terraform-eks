f3292a055255890e8c456e59094376252b3f01db
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-12-16 12:21:43 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "techevolv.fun"
+}
+
+variable "zone_id" {
+    default = "Z10407763GGQ6NFK3DU2A"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10407763GGQ6NFK3DU2A"
+}
+
+variable "domain_name" {
+    default = "techevolv.fun"
+}
