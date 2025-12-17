e524d3a32fc5ead3de8124fb52df70684fc2e400
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-12-17 13:51:35 +0530

    terraform

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -3,7 +3,8 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               = local.common_name_suffix
-  kubernetes_version = "1.33"
+  # kubernetes_version = "1.33"
+  kubernetes_version = var.eks_version
 
   addons = {
     coredns                = {}
@@ -17,10 +18,7 @@ module "eks" {
     metrics-server = {}
   }
 
-  # Optional
   endpoint_public_access = false
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = local.vpc_id
@@ -33,24 +31,10 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-    # blue = {
-    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-    #   ami_type       = "AL2023_x86_64_STANDARD"
-    #   instance_types = ["m5.xlarge"]
-    #   iam_role_additional_policies  = {
-    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #   }
-      
-    #   # cluster nodes autoscaling
-    #   min_size     = 2
-    #   max_size     = 10
-    #   desired_size = 2
-    # }
-
-    green = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+    blue = {
+      create = var.enable_blue
       ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_blue_version
       instance_types = ["m5.xlarge"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
@@ -69,6 +53,38 @@ module "eks" {
       #     effect = "NO_SCHEDULE"
       #   }
       # }
+
+      labels = {
+        nodegroup = "blue"
+      }
+    }
+
+    green = {
+      create = var.enable_green
+      ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_green_version
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+      
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
+
+      labels = {
+        nodegroup = "green"
+      }
     }
   }
 
--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -12,4 +12,25 @@ variable "zone_id" {
 
 variable "domain_name" {
     default = "techevolv.fun"
+
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
 }
